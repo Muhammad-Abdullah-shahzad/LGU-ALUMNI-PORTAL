4d71326af378bcebb0961ec5d46549b57821dbe1
MOAZAM ALI <moaxzam786@gmail.com>
2025-11-20 18:57:55 +0500

    Stat Card Issue Solved

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -5,8 +5,8 @@ exports.getAdminDashboard = async (req, res) => {
     const dashboardData = {};
     try {
         dashboardData.totalAlumni = await userModel.countDocuments({ role: 'alumni' });
-        dashboardData.totalEmployedAlumni = await userModel.countDocuments({ role: 'alumni', "details.employmentStatus": "employed" });
-        dashboardData.totalUnemployedAlumni = await userModel.countDocuments({ role: 'alumni', "details.employmentStatus": "unemployed" });
+        dashboardData.totalEmployedAlumni = await userModel.countDocuments({ role: 'alumni', "employmentStatus": "employed" });
+        dashboardData.totalUnemployedAlumni = await userModel.countDocuments({ role: 'alumni', "employmentStatus": "unemployed" });
         dashboardData.notifications = await notificationModel.find({
             $or: [
                 { notificationType: "editAlumni" },
@@ -22,7 +22,7 @@ exports.getAdminDashboard = async (req, res) => {
         ]);
 
         dashboardData.departmentWiseEmployed = await userModel.aggregate([
-            { $match: { role: 'alumni', "details.employmentStatus": "employed" } },
+            { $match: { role: 'alumni', "employmentStatus": "employed" } },
             { $group: { _id: "$department", count: { $sum: 1 } } },
             { $project: { department: "$_id", count: 1, _id: 0 } }
         ]);
